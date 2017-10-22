66c1b6bd422ba3893c5610b121076fea81de971d
leepolla <leepolla@gmail.com>
2017-10-22 10:40:45 -0700

    replace hover events

--- personal-bias-blocker.js
+++ personal-bias-blocker.js
@@ -15,6 +15,7 @@ var replacements = {
 };
 
 nodeReplace(document.body, dictionary);
+senseReplace();
 
 function nodeReplace(node, dictionary) {
   let child, next;
@@ -40,11 +41,38 @@ function textReplace(textNode, dictionary) {
   let content = textNode.innerHTML;
   dictionary.forEach(function(bias) {
     const regex = new RegExp(`\\b${bias}\\b`, 'i');
-    content = content.replace(regex, `<span class=' blocked ${dictionary.indexOf(bias)}'>${bias}</span>`);
+    content = content.replace(regex, `<span class='blocked ${dictionary.indexOf(bias)}'>${bias}</span>`);
   });
   Object.keys(replacements).forEach(function(bias) {
     const regex = new RegExp(`\\b${bias}\\b`, 'i');
-    content = content.replace(regex, `<span class=' replaced'>${replacements[bias]}</span>`);
+    content = content.replace(regex, `<span class='replaced' value='${bias}'>${replacements[bias]}</span>`);
+	textNode.value = bias.toString();
   });
   textNode.innerHTML = content;
+}
+
+function originalWord() {
+	
+}
+
+function senseReplace() {
+	var changes = document.querySelectorAll(".replaced");
+	//mouseover
+	changes.forEach(function(change) {
+		change.addEventListener("mouseover", function(){
+			var temp = change.innerHTML;
+			console.log(change.value);
+			change.innerHTML = change.attributes.value.value;
+			change.attributes.value.value = temp;
+		});
+	});
+	//mouseout
+	changes.forEach(function(change) {
+		change.addEventListener("mouseout", function(){
+			var temp = change.innerHTML;
+			console.log(change.value);
+			change.innerHTML = change.attributes.value.value;
+			change.attributes.value.value = temp;
+		});
+	});
 }
